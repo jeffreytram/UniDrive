3abe2f72e13ed681de1f2f9fc27b59aaf5717c9b
jeffreytram <jeffrey_tram@yahoo.com>
2020-09-02 05:40:58 -0400

    Improved code style in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Icon from './images/icon.png';
-import './Header.css'
+import './Header.css';
 
 export default function Header() {
-    return (
-        <div className="HeaderContainer">
-            <img className="Icon" src={Icon} alt = "Icon" />
-        </div>
-    )
+  return (
+    <div className="HeaderContainer">
+      <img className="Icon" src={Icon} alt="Icon" />
+    </div>
+  );
 }
