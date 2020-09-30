89ea319fb5f716bb92f96a016810afec0f82d525
RobertGiuffreda <rgiuffreda3@gatech.edu>
2020-09-30 19:24:15 -0400

    Upload fixes

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -12,8 +12,8 @@ class Upload extends Component {
       uploadFiles: []
     }
   }
+
   toggleUploadMenu = () => {
-    console.log("??");
     this.setState((prevState) => ({
       showUploadMenu: !prevState.showUploadMenu
     }));
@@ -21,7 +21,6 @@ class Upload extends Component {
   
   addFiles(target) {
     const list = [];
-    console.log(target.files);
     for (let i = 0; i < target.files.length; i++) {
       list[i] = {
         file: target.files[i],
@@ -33,79 +32,20 @@ class Upload extends Component {
     }));
   }
   uploadFiles = () => {
-    for (let i = 0; i < this.state.uploadFiles.length; i++) {
-      this.fileUpload((this.state.uploadFiles[i].user), this.state.uploadFiles[i].file);
+    const { uploadFiles } = this.state;
+    const { fileUpload } = this.props;
+    for (let i = 0; i < uploadFiles.length; i++) {
+      fileUpload((uploadFiles[i].user), uploadFiles[i].file);
     }
     this.setState({
       uploadFiles: []
     });
   }
 
-    /**
-   * Uploads a file specified
-   * @param {*} email User info for getting account
-   * @param {*} fileUpl File to be uploaded
-   */
-  fileUpload = (email, fileUpl) => {
-    /* const index = this.getAccountIndex(userId);
-    const email = this.parseIDToken(userId); */
-    window.gapi.client.load('drive', 'v3').then(() => {
-      window.gapi.auth2.authorize({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        scope: SCOPE,
-        prompt: 'none',
-        login_hint: email,
-        discoveryDocs: [discoveryUrl],
-      }, (response) => {
-        if (response.error) {
-          console.log(response.error);
-          console.log("Auth error.");
-        }
-        const file = fileUpl;
-        //new File(['Hello, world!'], 'hello world.txt', { type: 'text/plain;charset=utf-8' });
-        const contentType = file.type || 'application/octet-stream';
-        const resumable = new XMLHttpRequest();
-        resumable.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable', true);
-        resumable.setRequestHeader('Authorization', 'Bearer ' + response.access_token);
-        resumable.setRequestHeader('Content-Type', 'application/json');
-        resumable.setRequestHeader('X-Upload-Content-Length', file.size);
-        resumable.setRequestHeader('X-Upload-Content-Type', contentType);
-        console.log(resumable);
-        console.log("remsume");
-        resumable.onreadystatechange = function() {
-          if(resumable.readyState === XMLHttpRequest.DONE && resumable.status === 200) {
-            const locationUrl = resumable.getResponseHeader('Location');
-            const reader = new FileReader();
-            reader.onload = () => {
-              const uploadResumable = new XMLHttpRequest();
-              uploadResumable.open('PUT', locationUrl, true);
-              uploadResumable.setRequestHeader('Content-Type', contentType);
-              uploadResumable.setRequestHeader('X-Upload-Content-Type', contentType);
-              uploadResumable.onreadystatechange = function() {
-                if(uploadResumable.readyState === XMLHttpRequest.DONE && uploadResumable.status === 200) {
-                  console.log(uploadResumable.response);
-                }
-              };
-              uploadResumable.send(reader.result);
-              console.log("app");
-            };
-            reader.readAsArrayBuffer(file);
-          }
-        };
-        resumable.send(JSON.stringify({
-          'name': file.name,
-          'mimeType': contentType,
-          'Content-Type': contentType,
-          'Content-Length': file.size
-        }));
-      });
-    });
-  }
-
   render() {
+    const { showUploadMenu } = this.state;
     const uploadStyles = {
-      display: this.state.showUploadMenu ? 'flex' : 'none',
+      display: showUploadMenu ? 'flex' : 'none',
     };
     const {userList, parseIDToken} = this.props;
     return (
@@ -116,7 +56,7 @@ class Upload extends Component {
           id="upload-menu-btn" 
           onClick={() => this.toggleUploadMenu()}
           onKeyDown={() => this.toggleUploadMenu()}>
-          Uploado
+          Uploader
         </button>
           <div className="menu" style={uploadStyles}>
             {userList.map((user) => (
@@ -130,7 +70,6 @@ class Upload extends Component {
                   id={user.id}
                   onChange={(e) => this.addFiles(e.target)} 
                   multiple/>
-                <button type="button" onClick={() => this.toggleUploadMenu()}>Close</button>
               </div>
             ))}
           </div>
