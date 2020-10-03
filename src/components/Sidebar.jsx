f00c03a1693a1108f43eaa759519b478d431e24e
jeffreytram <jeffrey_tram@yahoo.com>
2020-10-03 14:28:57 -0400

    Created the sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="content">
+        <h1>Sidebar</h1>
+      </div>
+    </div>
+  );
+}
