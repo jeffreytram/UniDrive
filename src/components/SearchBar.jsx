824ff61ae46d445ca5605a37c3408532619cd03e
jeffreytram <jeffrey_tram@yahoo.com>
2020-11-04 11:47:14 -0500

    Fixed error, style changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,44 +1,41 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      searchInput: ''
-    }
-    this.inputUpdated = this.inputUpdated.bind(this);
-    this.submitSearchInput = this.submitSearchInput.bind(this);
+      searchInput: '',
+    };
   }
 
-  submitSearchInput(e) {
-    //e.preventDefault();
-
+  submitSearchInput = (e) => {
     const { searchInput } = this.state;
     const { onSubmit } = this.props;
-    onSubmit(searchInput)
-    this.setState({ searchInput:''});
+    onSubmit(searchInput);
+    this.setState({ searchInput: '' });
     return false;
   }
 
-  inputUpdated(e) {
+  inputUpdated = (e) => {
     const { value } = e.target;
 
     this.setState({ searchInput: value });
   }
+
   render() {
     return (
       <div className="search-form">
-        <form onSubmit={(e) => {e.preventDefault(); (e) => this.submitSearchInput()}}>
+        <form onSubmit={(e) => { e.preventDefault(); }}>
           <label htmlFor="search">Search By File Name </label>
           <input
             className="form-control"
             type="input"
             name="search"
             value={this.state.searchInput}
-            onInput={this.inputUpdated}/>
-            <button type="submit" onClick={() => this.submitSearchInput()}>Search</button>
+            onInput={this.inputUpdated}
+          />
+          <button type="submit" onClick={() => this.submitSearchInput()}>Search</button>
         </form>
       </div>
     );
